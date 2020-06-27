096a1d205c488c9bd330606081071b7280fe222a
Arthur Silva <root.arthur@gmail.com>
2020-06-26 22:16:57 -0300

    chore: add light and dark theme on styles

---
+++ src/shared/styles/themes/dark.ts
@@ -0,0 +1,8 @@
+export default {
+  title: 'dark',
+  colors: {
+    white: '#fff',
+    black: '#000',
+    dark: '#444444',
+  },
+}
---
+++ src/shared/styles/themes/light.ts
@@ -0,0 +1,8 @@
+export default {
+  title: 'light',
+  colors: {
+    white: '#fff',
+    black: '#000',
+    dark: '#444444',
+  },
+}
