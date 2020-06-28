72dd0d9d2f36ffe69d490db3b4ee1fbd9a6c3ee2
Arthur Silva <root.arthur@gmail.com>
2020-06-28 11:30:41 -0300

    feat: add components for the page/Main to work

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+
+import Scanner from './components/Scanner'
+import Widget from './components/Widget'
+import { Container } from './styles'
 
 const Main = () => {
+  const [isbn, setIsbn] = useState<number>()
+  const [scanError, setScanError] = useState<string>()
+
+  useEffect(() => {
+    if (scanError) {
+      console.log(scanError)
+    }
+  }, [scanError])
+
   return (
-    <div>
-      <h1>Main</h1>
-    </div>
+    <Container>
+      <Scanner receiveIsbn={setIsbn} receiveError={setScanError} />
+      {isbn && <Widget isbn={isbn} />}
+    </Container>
   )
 }
 
