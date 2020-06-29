e3150bb3956ac66d2952a62bf6c3c54fe23d34b0
Arthur Silva <root.arthur@gmail.com>
2020-06-29 00:12:26 -0300

    feat: add toastContainer on App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
+import { ToastContainer } from 'react-toastify'
 
 import ThemeProvider from './shared/contexts/theme'
 import Routes from './routes'
+import 'react-toastify/dist/ReactToastify.css'
 
 const App: React.FC = () => {
   return (
     <ThemeProvider>
       <Routes />
+      <ToastContainer />
     </ThemeProvider>
   )
 }
