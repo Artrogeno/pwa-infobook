4f7c2e14f79d41fb31a67b84a18c52fb0bd01590
Arthur Silva <root.arthur@gmail.com>
2020-06-29 00:13:21 -0300

    feat: adjust hooks useTheme

--- src/shared/hooks/useTheme.tsx
+++ src/shared/hooks/useTheme.tsx
@@ -1,11 +1,31 @@
-import { useContext, Dispatch, SetStateAction } from 'react'
+import {
+  useState,
+  useEffect,
+  useContext,
+  Dispatch,
+  SetStateAction,
+} from 'react'
+import { DefaultTheme } from 'styled-components'
 
 import { ThemeContext } from '../contexts/theme'
+import dark from '../styles/themes/dark'
+import light from '../styles/themes/light'
 
-type Response<T> = [T, Dispatch<SetStateAction<T>>]
+type Response<T> = [T, Dispatch<SetStateAction<T>>, DefaultTheme]
 
 export default function useTheme<T>(): Response<T> {
   const context = useContext(ThemeContext)
   const [theme, setTheme] = context
-  return [theme, setTheme]
+  const [type, setType] = useState(theme.title)
+
+  useEffect(() => {
+    const handleTheme = () => {
+      if (type !== theme.title) {
+        setTheme(type === 'light' ? light : dark)
+      }
+    }
+    handleTheme()
+  })
+
+  return [type, setType, theme]
 }
