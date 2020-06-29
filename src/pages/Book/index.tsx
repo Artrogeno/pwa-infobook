6548182c9370c4fb4b6e080f394f0f0b77504fd8
Arthur Silva <root.arthur@gmail.com>
2020-06-29 00:21:56 -0300

    feat: adjust layout and find book

--- src/pages/Book/index.tsx
+++ src/pages/Book/index.tsx
@@ -1,12 +1,138 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
 
-import { Container } from './styles'
+import img404 from '../../assets/images/404.jpg'
+import StarRating from '../../shared/components/StarRating'
+import Header from './components/Header'
+import { findBook } from '../../shared/services/books'
+import { BookItemI } from '../../shared/interfaces/book'
+import {
+  Container,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Title,
+  Subtitle,
+  Label,
+  LabelSmall,
+  LabelLink,
+  Info,
+  Img,
+  Description,
+  Text,
+  Starts,
+  Rate,
+  RateSmall,
+} from './styles'
 
 const Book = () => {
+  let { isbn } = useParams()
+
+  const [book, setBook] = useState<BookItemI>()
+
+  useEffect(() => {
+    const loadBook = async () => {
+      if (isbn) {
+        const response = await findBook(isbn)
+        if (response.totalItems > 0) {
+          if (response.items && response.items.length > 0) {
+            setBook(response.items[0])
+          }
+        }
+      }
+    }
+    loadBook()
+  }, [isbn])
+
   return (
-    <Container>
-      <h2>Test Book</h2>
-    </Container>
+    <>
+      <Header />
+      <Container>
+        {book && (
+          <Card>
+            <CardHeader>
+              <Title>{book.volumeInfo.title}</Title>
+              <Subtitle>{book.volumeInfo.subtitle}</Subtitle>
+            </CardHeader>
+            <CardBody>
+              <Info>
+                {book.volumeInfo.imageLinks ? (
+                  <Img
+                    src={book.volumeInfo.imageLinks.smallThumbnail}
+                    alt="book"
+                  />
+                ) : (
+                  <Img src={img404} alt="book" />
+                )}
+                <Description>
+                  {book.volumeInfo.authors && (
+                    <Label>
+                      Author:
+                      {book.volumeInfo.authors.map((author, i) => (
+                        <LabelSmall key={i}>
+                          {author}{' '}
+                          {book.volumeInfo.authors &&
+                          book.volumeInfo.authors.length - 1 === i
+                            ? ''
+                            : ','}
+                        </LabelSmall>
+                      ))}
+                    </Label>
+                  )}
+                  <Label>
+                    Originally published:
+                    <LabelSmall>{book.volumeInfo.publishedDate}</LabelSmall>
+                  </Label>
+                  <Label>
+                    Pages:
+                    <LabelSmall>{book.volumeInfo.pageCount}</LabelSmall>
+                  </Label>
+                  {book.volumeInfo.categories && (
+                    <Label>
+                      Category:
+                      {book.volumeInfo.categories.map((category, i) => (
+                        <LabelSmall key={i}>
+                          {category}{' '}
+                          {book.volumeInfo.categories &&
+                          book.volumeInfo.categories.length - 1 === i
+                            ? ''
+                            : ','}
+                        </LabelSmall>
+                      ))}
+                    </Label>
+                  )}
+                  <Label>
+                    Info Link:
+                    <LabelLink href={book.volumeInfo.infoLink} target="_blank">
+                      link
+                    </LabelLink>
+                  </Label>
+                </Description>
+              </Info>
+              <Description className="no-p">
+                <Text>{book.volumeInfo.description}</Text>
+              </Description>
+            </CardBody>
+            {book.volumeInfo.ratingsCount && (
+              <CardFooter>
+                <Title>Rate and review</Title>
+                <Starts>
+                  <StarRating
+                    rating={book.volumeInfo.ratingsCount}
+                    averageRating={book.volumeInfo.averageRating}
+                  />
+                  <Rate>
+                    {book.volumeInfo.ratingsCount}
+                    <RateSmall>/{book.volumeInfo.averageRating}</RateSmall>
+                  </Rate>
+                </Starts>
+              </CardFooter>
+            )}
+          </Card>
+        )}
+      </Container>
+    </>
   )
 }
 
--- src/pages/Book/styles.tsx
+++ src/pages/Book/styles.tsx
@@ -1,3 +1,101 @@
 import styled from 'styled-components'
 
-export const Container = styled.section``
+export const Container = styled.section`
+  position: relative;
+  padding: 5rem 1rem 1rem 1rem;
+`
+
+export const Card = styled.div``
+
+export const CardHeader = styled.div`
+  padding-bottom: 1rem;
+  border-bottom: 0.01rem solid ${props => props.theme.colors.border};
+`
+
+export const CardBody = styled.div`
+  padding: 1rem 0;
+  border-bottom: 0.01rem solid ${props => props.theme.colors.border};
+`
+
+export const CardFooter = styled.div`
+  padding: 1rem 0;
+`
+
+export const Title = styled.h2`
+  line-height: 1.4rem;
+  font-weight: 400;
+  padding-bottom: 0.8rem;
+`
+
+export const Subtitle = styled.h5`
+  font-family: var(--font-raleway);
+  font-weight: 200;
+  padding-bottom: 1.4rem;
+`
+
+export const Label = styled.h6`
+  font-weight: bold;
+  margin-bottom: 0.6rem;
+`
+
+export const LabelSmall = styled.small`
+  font-size: 0.8rem;
+  font-weight: 400;
+  font-family: var(--font-raleway);
+  margin-left: 0.4rem;
+`
+
+export const LabelLink = styled.a`
+  text-decoration: none;
+  cursor: pointer;
+  font-size: 0.8rem;
+  font-weight: 400;
+  font-family: var(--font-raleway);
+  margin-left: 0.4rem;
+  padding: 0.1rem 0.4rem;
+  border-radius: 0.1rem;
+  background-color: ${props => props.theme.colors.primary};
+  color: ${props => props.theme.colors.white};
+`
+
+export const Info = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: flex-start;
+`
+
+export const Img = styled.img`
+  width: 8rem;
+  height: auto;
+`
+
+export const Description = styled.div`
+  padding: 0 1rem;
+  &.no-p {
+    padding: 0;
+  }
+`
+
+export const Text = styled.p`
+  margin: 1rem 0;
+  font-size: 0.8rem;
+  line-height: 1.2rem;
+`
+
+export const Starts = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+  padding-bottom: 0.6rem;
+`
+
+export const Rate = styled.h4`
+  margin-left: 0.6rem;
+`
+
+export const RateSmall = styled.small`
+  font-size: 0.8rem;
+  opacity: 0.6;
+`
